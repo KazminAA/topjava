71bf5066cf2e54b3b89fe11e74d35880f8e98197
Alexandr <lleeexxx@gmail.com>
2017-12-04 15:44:35 +0200

    most correct imlementation of jdbcmealepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/AbstractJdbcMealRepositoryImpl.java
@@ -12,14 +12,15 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public abstract class AbstractJdbcMealRepositoryImpl implements MealRepository {
 
-    protected static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+    private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
-    protected final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
@@ -36,15 +37,21 @@ public abstract class AbstractJdbcMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal save(Meal meal, int userId) {
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("description", meal.getDescription())
+                .addValue("calories", meal.getCalories())
+                .addValue("date_time", getTimestamp(meal.getDateTime()))
+                .addValue("user_id", userId);
         if (meal.isNew()) {
-            Number newId = insertMeal.executeAndReturnKey(getMap(meal, userId));
+            Number newId = insertMeal.executeAndReturnKey(map);
             meal.setId(newId.intValue());
         } else {
             if (namedParameterJdbcTemplate.update("" +
                             "UPDATE meals " +
                             "   SET description=:description, calories=:calories, date_time=:date_time " +
                             " WHERE id=:id AND user_id=:user_id"
-                    , getMap(meal, userId)) == 0) {
+                    , map) == 0) {
                 return null;
             }
         }
@@ -69,5 +76,12 @@ public abstract class AbstractJdbcMealRepositoryImpl implements MealRepository {
                 "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
     }
 
-    protected abstract MapSqlParameterSource getMap(Meal meal, int userId);
+    @Override
+    public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
+        return jdbcTemplate.query(
+                "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId, getTimestamp(startDate), getTimestamp(endDate));
+    }
+
+    public abstract <T> T getTimestamp(LocalDateTime dateTime);
 }
