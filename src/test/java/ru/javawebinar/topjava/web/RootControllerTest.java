1cce2486d29b30e45127a61f7214d51caa9b5a71
Alexandr <lleeexxx@gmail.com>
2017-12-26 14:26:07 +0200

    HW7 1.1 and 1.2 add test for root controller meals add test for respurce controller

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,10 +2,15 @@ package ru.javawebinar.topjava.web;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.MEAL5;
+import static ru.javawebinar.topjava.MealTestData.MEALS;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
@@ -23,6 +28,24 @@ public class RootControllerTest extends AbstractControllerTest {
                         allOf(
                                 hasProperty("id", is(START_SEQ)),
                                 hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+
+    @Test
+    public void tetsMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(MEALS.size())))
+                .andExpect(model().attribute("meals", hasItem(
+                        allOf(
+                                hasProperty("id", is(MEAL5.getId())),
+                                hasProperty("dateTime", is(LocalDateTime.of(2015, Month.MAY, 31, 13, 0))),
+                                hasProperty("description", is(MEAL5.getDescription())),
+                                hasProperty("calories", is(1000))
                         )
                 )));
     }
